2a750b7cb71ea098a84e4f0d790bc032bf9074c1
jolop1 <patrickoliver.jolo@students.bfh.ch>
2016-12-02 22:02:40 +0100

    Vaadin Test

--- src/main/java/ch/bfh/btx/blue/Test/MyUI.java
+++ src/main/java/ch/bfh/btx/blue/Test/MyUI.java
@@ -13,37 +13,53 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of a html page where a Vaadin application is
+ * embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
  */
 @Theme("mytheme")
 public class MyUI extends UI {
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
 
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        layout.setMargin(true);
-        layout.setSpacing(true);
-        
-        setContent(layout);
-    }
+		final VerticalLayout layout = new VerticalLayout();
+		setContent(layout);
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+		final TextField name = new TextField();
+		name.setCaption("Type your name here:");
+		TextField t1 = new TextField();
+		t1.setCaption("Test");
+
+		Button button = new Button("Click Me");
+		Button b2 = new Button("Jolop1");
+
+		b2.addClickListener(e -> {
+			layout.addComponent(new Label("Test von Patrick"));
+		});
+		button.addClickListener(e -> {
+			layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
+		});
+
+		layout.addComponents(b2, name, button, t1);
+
+		layout.setMargin(true);
+		layout.setSpacing(true);
+
+
+		
+		
+		
+
+		
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
 }
