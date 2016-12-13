cc5ab827163d04e5901643304f232f6eab5ad341
kammf1 <fabianstefan.kammermann@students.bfh.ch>
2016-12-13 10:20:13 +0100

    Patient.java bearbeitet

--- src/main/java/ch/bfh/btx/blue/adimed/businessLayer/Patient.java
+++ src/main/java/ch/bfh/btx/blue/adimed/businessLayer/Patient.java
@@ -1,21 +1,25 @@
 package ch.bfh.btx.blue.adimed.businessLayer;
 
 import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Patient extends Person {
-
+	@Id
+	private int pId;
+	private int cId;
 	private String insurance;
-	private int insuranceNr;
-	
+	private int insuranceNb;
 	private String allergies;
 
 	public Patient() {
 
 	}
 	
-	public Patient (String insurance, int insuranceNr, String allergies) {
+	public Patient (String insurance, int insuranceNb, String allergies) {
 		this.insurance = insurance;
-		this.insuranceNr = insuranceNr;
+		this.insuranceNb = insuranceNb;
 		this.allergies = allergies;
 	}
 
