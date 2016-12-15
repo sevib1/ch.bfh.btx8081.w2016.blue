495ae01856452ee3ee0cab149bb87087772cf5c7
varan2 <niveadha.kanagarasa@students.bfh.ch>
2016-12-15 10:30:50 +0100

    Case

--- src/main/java/ch/bfh/btx/blue/adimed/businessLayer/Case.java
+++ src/main/java/ch/bfh/btx/blue/adimed/businessLayer/Case.java
@@ -1,12 +1,18 @@
 package ch.bfh.btx.blue.adimed.businessLayer;
 
+import java.util.GregorianCalendar;
 import java.util.List;
 
-public class Case {
-
-	private int caseID;
-	
-	private List<Patient> patient; // Association
+import javax.persistence.Id;
+import javax.persistence.Entity;
+@Entity public class Case {
+@Id
+	private int CID;
+	private GregorianCalendar diagnoseDate;
+	private GregorianCalendar applyDate;
+	private GregorianCalendar laborDate;
+	private GregorianCalendar agendaDate;
+	private Patient patient; // Association
 	private List<LaborResult> laborResult; // Association
 	private List<Medication> medication; // Association
 	private List<Diagnosis> diagnosis; // Association
