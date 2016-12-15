f32532e9fcb1bdb2555e61503939f6366caa7236
varan2 <niveadha.kanagarasa@students.bfh.ch>
2016-12-15 13:59:09 +0100

    MedicationID

--- src/main/java/ch/bfh/btx/blue/adimed/businessLayer/Medication.java
+++ src/main/java/ch/bfh/btx/blue/adimed/businessLayer/Medication.java
@@ -9,7 +9,7 @@ import javax.persistence.Entity;
 
 
 
-@Entity
+@Entity @IdClass(MedicationId.class)
 public class Medication {
 	
 	@Id
---
+++ src/main/java/ch/bfh/btx/blue/adimed/businessLayer/MedicationId.java
@@ -0,0 +1,11 @@
+package ch.bfh.btx.blue.adimed.businessLayer;
+
+import javax.persistence.Id;
+
+public class MedicationId {
+
+	private int CID;	
+	private int applyDate;
+		
+
+}
