71377ea337c767eb3c57596fb99eb474da9ba108
jolop1 <patrickoliver.jolo@students.bfh.ch>
2016-12-15 13:43:02 +0100

    Medication anpassung

--- src/main/java/ch/bfh/btx/blue/adimed/businessLayer/Medication.java
+++ src/main/java/ch/bfh/btx/blue/adimed/businessLayer/Medication.java
@@ -3,17 +3,36 @@ package ch.bfh.btx.blue.adimed.businessLayer;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-public class Medication {
+import javax.persistence.Id;
+import javax.persistence.Entity;
 
-	private GregorianCalendar dateOfgivenMedical;
-	private String mediName;
-	private String mediDosis;
-	private boolean mediState;
-	private String applicationForm;
-	private String takingForm;
-	private String comment;
+
+
+
+
+public class Medication {
 	
+	@Id
+	private int CID;	
+	@Id
+	private int applyDate;
+		
+	private String medName;
+	
+	private String medDosis;
+	
+	private String medStatus;
+	
+	private String applyForm;
+	
+	private String comment;	
+	
+	
+
 	private Compendium compendium; // Association
+	private List<Staff> Staff; // Association
+
+	
 	private List<Staff> ServiceProviders; // Association
 	private List<Staff> Staffs; // Association
 
@@ -23,13 +42,13 @@ public class Medication {
 
 	public Medication(GregorianCalendar dateOfgivenMedical, String mediName, String mediDosis, boolean mediState,
 			Compendium compendium, String applicationForm, String takingForm, String comment) {
-		this.dateOfgivenMedical = dateOfgivenMedical;
-		this.mediName = mediName;
-		this.mediDosis = mediDosis;
-		this.mediState = mediState;
+		this.applyDate = applyDate;
+		this.medName = medName;
+		this.medDosis = medDosis;
+		this.medStatus = medStatus;
 		this.compendium = compendium;
-		this.applicationForm = applicationForm;
-		this.takingForm = takingForm;
+		this.applyForm = applicationForm;
+		//this.takingForm = takingForm;
 		this.comment = comment;
 
 	}
