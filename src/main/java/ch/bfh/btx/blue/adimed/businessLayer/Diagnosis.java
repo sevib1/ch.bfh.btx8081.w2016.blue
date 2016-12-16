9f4a9b06d97d0d0a12eb14de90d8395944994c34
lamlr1 <remy.lam@students.bfh.ch>
2016-12-16 09:56:42 +0100

    update Diagnosis

--- src/main/java/ch/bfh/btx/blue/adimed/businessLayer/Diagnosis.java
+++ src/main/java/ch/bfh/btx/blue/adimed/businessLayer/Diagnosis.java
@@ -2,7 +2,16 @@ package ch.bfh.btx.blue.adimed.businessLayer;
 
 import java.util.GregorianCalendar;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Diagnosis {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	
 	private GregorianCalendar diagnosisDate;
 	private String status;
